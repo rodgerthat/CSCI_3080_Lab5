172223471f747c8d75ee91d776029baae39ed70a
joelrnorris <joel.r.norris@gmail.com>
2017-03-28 12:56:29 -0500

    added adjacency matrix translation, needs work

--- lab5.cpp
+++ lab5.cpp
@@ -14,7 +14,8 @@ int main()
 {
 	const int MAX_ROWS = 30;
 	const int MAX_COLS = 2;
-	int A[MAX_ROWS][MAX_COLS] = { 0 };
+	int A[MAX_ROWS][MAX_COLS] = { 0 };		// array pointer representation
+	int AM[MAX_ROWS][MAX_ROWS] = { 0 };		// adjacency matrix
 	int numNodes = 0;
 	int numRows = 0;
 	int isolatedNodesArray[MAX_ROWS] = { 0 };
@@ -43,8 +44,10 @@ int main()
     }
 
 	// print out array-pointer representation
-	cout << "numNodes: " << numNodes << endl;
-	cout << "numRows: " << numRows << endl;
+	cout << endl << "--------------" << endl;
+	//cout << "numNodes: " << numNodes << endl;
+	//cout << "numRows: " << numRows << endl;
+	cout << endl;
     for (i = 0; i < numRows; ++i) {
 		for (j = 0; j < MAX_COLS; ++j) {
             cout << A[i][j] << " ";
@@ -55,10 +58,10 @@ int main()
 
 	// determine the isolated nodes, 
 	// those will be in the initial portion of the array, within numNodes,  w/ a -1.
-	cout << "--------------" << endl;
+	//cout << endl << "--------------" << endl;
 	for (i = 0; i < numNodes; ++i) {
 		// if this node is -1, it's isolated.
-		cout << "A[i][1]: " << A[i][1] << endl;
+		//cout << "A[i][1]: " << A[i][1] << endl;
 		if (A[i][1] == -1) {
 			// put it in the isolatedNodeArray
 			isolatedNodesArray[numIsolatedNodes] = A[i][0];
@@ -77,6 +80,36 @@ int main()
 		cout << "node " << isolatedNodesArray[0] << " is isolated" << endl;
 	}
 
+	// build an adjacency matrix based on the array-pointer representation
+	for (i = 0; i < numNodes; ++i) {
+
+		// prime the list traverse w/ the first values in the list
+		int nodeValue = A[ A[i][1] - 1 ][0];			// prep the initial node value
+		int	nextNode = A[ A[i][1] - 1 ][1];				// minus one, because mathematicians.
+		cout << "nodeValue: " << nodeValue << endl;
+		cout << "nextNode: " << nextNode << endl;
+
+
+		// if we're not at the end of the list, there's an adjacency
+		while (nextNode != -1) {
+
+			AM[i][nodeValue] = 1;	// update the adjacency matrix
+			
+			// move to next node in list
+			nextNode = A[nextNode][1] - 1;
+			nodeValue = A[nextNode][0];
+
+		}
+	}
+
+	// print out the adjacency matrix
+	cout << endl << "--------------" << endl;
+	for (i = 0; i < numNodes; ++i) {
+		for (j = 0; j < numNodes; ++j) {
+			cout << AM[i][j] << " ";
+		}
+		cout << endl;
+	}
 
     return 0;
 }
